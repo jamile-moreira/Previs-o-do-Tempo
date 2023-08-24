d39e953b7348838a6e889eafb8d3f1934834eb91
Jamile Moreira <jamilemoreira2310@gmail.com>
2023-08-24 16:44:07 -0300

    estrura para api consultar clima

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,22 @@ import { useState, useEffect } from 'react'
 
 const App = () => {
   const [nomeDaCidade, setNomeDaCidade] = useState('')
+  const [dadosDoClima, setDadosDoClima] = useState('')
+
+  const pegarDadosDoClima = async () => {
+    try {
+      const response = await fetch(
+        `https://wttr.in/${nomeDaCidade}?format=%t+%C+%w+%m`
+      )
+      const data = await response.text()
+      setDadosDoClima(data)
+    } catch (error) {
+      console.error('Erro ao pegar os dados:', error)
+    }
+  }
   useEffect(() => {
-    console.log(nomeDaCidade)
-  }, [])
+    pegarDadosDoClima()
+  }, [nomeDaCidade])
 
   return (
     <div>
